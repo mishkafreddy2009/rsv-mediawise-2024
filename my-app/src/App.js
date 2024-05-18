025e29af367ae46ba7fddfa9e00b61b8c751445b
ayurckov <yurckovama@gmail.com>
2024-05-18 11:21:38 +0500

    basic-front-page

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/header';
+import Dropzone from './components/dropzone/Dropzone';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dropzone />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/dropzone/Dropzone.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import styles from './Dropzone.module.css'
+
+const Dropzone = () => {
+    const [drag, setDrag] = useState(false);
+
+    function dragStartHandler(e) {
+        e.preventDefault()
+        setDrag(true)
+    }
+
+    function dragLeaveHandler(e) {
+        e.preventDefault()
+        setDrag(false)
+    }
+
+    function onDropHandler(e) {
+        e.preventDefault()
+        let files = [...e.dataTransfer.files]
+
+        const formData = new FormData()
+        // formData.append('file', files[0])
+        // axios.post('url',formData)
+
+        setDrag(false)
+    }
+
+    return (
+        <div className={styles.dropzone}>
+            {drag 
+            ? <div className={styles.dropArea}
+                onDragStart={e => dragStartHandler(e)}
+                onDragLeave={e => dragLeaveHandler(e)}
+                onDragOver={e =>dragStartHandler(e)}
+                onDrop={e => onDropHandler(e)}
+            >Отпустите видео</div>
+            : <div className={styles.NotDropArea}
+                onDragStart={e => dragStartHandler(e)}
+                onDragLeave={e => dragLeaveHandler(e)}
+                onDragOver={e =>dragStartHandler(e)}
+            >Перетащите файлы</div>}
+        </div>
+    )
+  
+}
+
+export default Dropzone;
